680e2d9ed109ce76d77a09eb13cbfd1a378e9fc7
blurname <naughtybao@outlook.com>
2023-03-22 21:31:45 +0800

    CHG: nightfox: style

--- lua/config/nightfox.lua
+++ lua/config/nightfox.lua
@@ -5,7 +5,7 @@ local nightfox = require('nightfox')
 nightfox.setup({
   options = {
   styles = {
-    comments = "italic", -- change style of comments to be italic
+    --comments = "italic", -- change style of comments to be italic
 		keywords = "bold", -- change style of keywords to be bold
 		functions = "italic,bold", -- styles can be a comma separated list
   },
