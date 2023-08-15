c3928b5874b56033d0999513cab2a42eb882b3dc
blurname <naughtybao@outlook.com>
2023-08-15 22:03:12 +0800

    CHG: better close

--- lua/m-tabline.lua
+++ lua/m-tabline.lua
@@ -1,5 +1,5 @@
-local tabline = require'tabline.setup'
-local h = require'tabline.helpers'
+local tabline = require('tabline.setup')
+local h = require('tabline.helpers')
 local k = vim.keymap
 
 vim.o.showtabline = 2
@@ -34,21 +34,11 @@ function close_tab_or_buffer()
 end
 
 function close_buffer()
-  -- 获取当前 buffer 列表
-  local buffers = vim.api.nvim_list_bufs()
-  local windows = vim.api.nvim_tabpage_list_wins(0)
-  -- 计算实际的 buffer 数量
-  local buffer_count = #buffers
-  local window_count = #windows
-
-  -- 如果只有一个 buffer，则不执行关闭操作
-  if buffer_count <= 1 then
-    print("---无法关闭最后一个 buffer")
-    return
-  end
 
   -- 先看 window 数量，防止同一个 buffer 开多个 window 的情况下，全被删了
-  if window_count >1 then
+  local currentBufWindowNum = #vim.fn.win_findbuf(vim.fn.winbufnr(0))
+
+  if currentBufWindowNum >1 then
     vim.cmd('q')
     return
   end
