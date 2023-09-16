5524dd235cef298051583c3018a5085f4f19dc0d
blurname <naughtybao@outlook.com>
2023-09-17 01:17:49 +0800

    CHG: bqf: better name

--- lua/config/bqf.lua
+++ lua/config/bqf.lua
@@ -24,8 +24,8 @@ function _G.qftf(info)
     else
         items = fn.getloclist(info.winid, {id = info.id, items = 0}).items
     end
-    local limit = 31
-    local fnameFmt1, fnameFmt2 = '%-' .. limit .. 's', '…%.' .. (limit - 1) .. 's'
+    -- local limit = 31
+    -- local fnameFmt1, fnameFmt2 = '%-' .. limit .. 's', '…%.' .. (limit - 1) .. 's'
     local validFmt = '%s │%5d:%-3d│%s %s'
     for i = info.start_idx, info.end_idx do
         local e = items[i]
@@ -34,17 +34,17 @@ function _G.qftf(info)
         if e.valid == 1 then
             if e.bufnr > 0 then
                 fname = fn.bufname(e.bufnr)
-                if fname == '' then
-                    fname = '[No Name]'
-                else
-                    fname = fname:gsub('^' .. vim.env.HOME, '~')
-                end
+                -- if fname == '' then
+                --     fname = '[No Name]'
+                -- else
+                --     fname = fname:gsub('^' .. vim.env.HOME, '~')
+                -- end
                 -- char in fname may occur more than 1 width, ignore this issue in order to keep performance
-                if #fname <= limit then
-                    fname = fnameFmt1:format(fname)
-                else
-                    fname = fnameFmt2:format(fname:sub(1 - limit))
-                end
+                -- if #fname <= limit then
+                --     fname = fnameFmt1:format(fname)
+                -- else
+                --     fname = fnameFmt2:format(fname:sub(1 - limit))
+                -- end
             end
             local lnum = e.lnum > 99999 and -1 or e.lnum
             local col = e.col > 999 and -1 or e.col
@@ -58,7 +58,7 @@ function _G.qftf(info)
     return ret
 end
 
--- vim.o.qftf = '{info -> v:lua._G.qftf(info)}'
+vim.o.qftf = '{info -> v:lua._G.qftf(info)}'
 
 
 
