16ab2106d549af61ae546ada98e6773516546ead
blurname <naughtybao@outlook.com>
2024-03-09 15:21:47 +0800

    CHG: kbd: git related

--- lua/toggleterm-m.lua
+++ lua/toggleterm-m.lua
@@ -15,7 +15,7 @@ function _yazi_toggle()
   yazi:toggle()
 end
 
-vim.api.nvim_set_keymap("n", "<leader>gg", "<cmd>lua _lazygit_toggle()<CR>", {noremap = true, silent = true})
+vim.api.nvim_set_keymap("n", "<tab>", "<cmd>lua _lazygit_toggle()<CR>", {noremap = true, silent = true})
 vim.api.nvim_set_keymap("n", "<leader>gf", "<cmd>lua _yazi_toggle()<CR>", {noremap = true, silent = true})
 
 local saved_terminal
@@ -23,27 +23,27 @@ local saved_terminal
 require("flatten").setup({
   window = {
     open = "alternate",
-  },
-        callbacks = {
-        pre_open = function()
-          -- _lazygit_toggle()
-          local term = require("toggleterm.terminal")
-          local termid = term.get_toggled_id(1)
-          
-          local all = term.get_all(true)
-          for index, termm in pairs(all) do
-            -- if index ~= term.id then return index end
-          -- print(index)
-          -- saved_terminal = term.get(index)
-          saved_terminal = termm
-          termm:close()
-          end
-          if saved_terminal then
-            if api.nvim_buf_is_loaded(saved_terminal.bufnr) then
-              api.nvim_buf_delete(saved_terminal.bufnr, { force = true })
-            end
-          end
-        end,
-      },
+  }
+      --   callbacks = {
+      --   pre_open = function()
+      --     -- _lazygit_toggle()
+      --     local term = require("toggleterm.terminal")
+      --     local termid = term.get_toggled_id(1)
+      --     
+      --     local all = term.get_all(true)
+      --     for index, termm in pairs(all) do
+      --       -- if index ~= term.id then return index end
+      --     -- print(index)
+      --     -- saved_terminal = term.get(index)
+      --     saved_terminal = termm
+      --     termm:close()
+      --     end
+      --     if saved_terminal then
+      --       if api.nvim_buf_is_loaded(saved_terminal.bufnr) then
+      --         api.nvim_buf_delete(saved_terminal.bufnr, { force = true })
+      --       end
+      --     end
+      --   end,
+      -- },
 })
 
--- vim/leaderf.vim
+++ vim/leaderf.vim
@@ -55,10 +55,10 @@ let g:Lf_NormalMap = {
 " noremap <C-F> :<C-U><C-R>=printf("Leaderf! rg --right --stayOpen -e %s ", expand("<cword>"))<CR><CR>
 " vnoremap ff :<C-U><C-R>=printf("Leaderf! rg --right --stayOpen -e %s ", leaderf#Rg#visual())<CR><CR>
 nnoremap <leader>f; :Leaderf! cmdHistory<CR>
-nnoremap  <tab> :Leaderf! --recall<CR>
+nnoremap <leader>j :Leaderf! --recall<CR>
 nnoremap <leader>b :Leaderf! buffer<CR>
 " nnoremap <leader>j :Telescope jumplist initial_mode=normal<CR>
-nnoremap <leader>j :Leaderf! jumps<CR>
+" nnoremap <leader>j :Leaderf! jumps<CR>
 nnoremap <leader>fh :Telescope help_tags<CR>
 " noremap <c-j> :Leaderf --next<CR>
 " noremap <c-k> :Leaderf --previous<CR>
