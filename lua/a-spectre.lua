79dfa64a2abe6493322582165a6a8a5e96d9ea59
blurname <naughtybao@outlook.com>
2024-04-13 16:20:42 +0800

    CHG: nvim-spectre: rg -F

--- lua/a-spectre.lua
+++ lua/a-spectre.lua
@@ -14,7 +14,36 @@ require('spectre').setup({
       replace = "@text.danger"
   },
   lnum_UI = 20,
-  line_result = 20
+  line_result = 20,
+    find_engine = {
+    -- rg is map with finder_cmd
+    ['rg'] = {
+      cmd = "rg",
+      -- default args
+      args = {
+        '--color=never',
+        '--no-heading',
+        '--with-filename',
+        '--line-number',
+        '--column',
+        '-F'
+      } ,
+      options = {
+        ['ignore-case'] = {
+          value= "--ignore-case",
+          icon="[I]",
+          desc="ignore case"
+        },
+        ['hidden'] = {
+          value="--hidden",
+          desc="hidden file",
+          icon="[H]"
+        },
+        -- you can put any rg search option you want here it can toggle with
+        -- show_option function
+      }
+    }
+  }
 })
 
 -- 1wincmd l
