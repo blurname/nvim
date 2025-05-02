26b1fd33dbcc5f5518c6b72970eb2d3268aaa743
blurname <naughtybao@outlook.com>
2025-05-03 01:26:55 +0800

    WIP; git

--- lua/config/gitsigns.lua
+++ lua/config/gitsigns.lua
@@ -60,8 +60,8 @@ require('gitsigns').setup {
     -- Actions
     map({'n', 'v'}, '<leader>hs', ':Gitsigns stage_hunk<CR>')
     map({'n', 'v'}, '<leader>hr', ':Gitsigns reset_hunk<CR>')
-    -- map('n', 'gj', ':Gitsigns next_hunk<CR>')
-    -- map('n', 'gk', ':Gitsigns prev_hunk<CR>')
+    map('n', 'gj', ':Gitsigns next_hunk<CR>')
+    map('n', 'gk', ':Gitsigns prev_hunk<CR>')
     --map('n', '<leader>hS', gs.stage_buffer)
     -- map('n', '<leader>hu', gs.undo_stage_hunk)
     --map('n', '<leader>hR', gs.reset_buffer)
